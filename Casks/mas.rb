bff750780c74521112a1cfbd99324e749c94b161
norifumi-okumura <110379628+norifumi-okumura@users.noreply.github.com>
2026-03-15 20:28:57 +0900

    Add Mas cask v3.9.0

---
+++ Casks/mas.rb
@@ -0,0 +1,15 @@
+cask "mas" do
+  version "3.9.0"
+  sha256 "8e489b2e88fd30a26b34b4e3bdb560ad61f2c599d15578f28eea30c45ee0d784"
+
+  url "https://github.com/piggest/Mas/releases/download/v#{version}/Mas-#{version}.zip"
+  name "Mas"
+  desc "Screenshot and screen recording tool"
+  homepage "https://github.com/piggest/Mas"
+
+  app "Mas.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.example.Mas.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-mas
+
+Homebrew tap for [Mas](https://github.com/piggest/Mas) - Screenshot & Screen Recording app for macOS.
+
+## Install
+
+```bash
+brew tap piggest/mas
+brew install --cask mas
+```
+
+## Update
+
+```bash
+brew upgrade --cask mas
+```
