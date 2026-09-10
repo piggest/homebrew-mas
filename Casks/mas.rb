dc72f721c96f576df04ba91f66187a906fa42ca4
Norifumi Okumura <piggest_no0965@hotmail.com>
2026-09-10 14:40:13 +0900

    Update mas to v5.4.5

--- Casks/mas.rb
+++ Casks/mas.rb
@@ -1,6 +1,6 @@
 cask "mas" do
-  version "5.4.0"
-  sha256 "a63e6924769cad40aa97836436bc32f217ee9cde819050388132a01d6c7963f8"
+  version "5.4.5"
+  sha256 "84e02466ef74530596451fc3b57c92d987f161013726e90f155db8827fa55b68"
 
   url "https://github.com/piggest/Mas/releases/download/v#{version}/Mas-v#{version}.dmg"
   name "Mas"
